b95352cb87f7b250db5c68e96a7633505cff3b3e
Edney <edney.filho@canoo.com>
2024-10-18 18:57:55 -0300

    feat: fix default plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,50 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: 'tsconfig.json',
-      tsconfigRootDir: __dirname,
-      sourceType: 'module',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [".eslintrc.cjs", "commitlint.config.ts"],
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
     },
-    plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    root: true,
-    env: {
-      node: true,
-      jest: true,
+    {
+      files: ['*.js'],
+      parser: 'espree',
+      parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: 'module',
+      },
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'auto',
+            singleQuote: true,
+            printWidth: 100,
+            arrowParens: 'avoid',
+            bracketSpacing: true,
+          },
+        ],
+      },
     },
-    ignorePatterns: [".eslintrc.cjs", "commitlint.config.ts"],
-    rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      "prettier/prettier": [
-        "error",
-        {
-          "endOfLine": "auto",
-          "singleQuote": true,
-          "printWidth": 100,
-          "arrowParens": "avoid",
-          "bracketSpacing": true
-        }
-      ],
-    },
-  };
-  
+  ],
+};
