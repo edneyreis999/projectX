4164aa8e108bbcabd0b65cd357629f1b39360163
Edney <edney_reis999@hotmail.com>
2025-08-31 13:02:19 -0300

    refactor: remove unused Jest configuration file

--- frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/js'],
-  collectCoverageFrom: ['js/plugins/**/*.js', '!js/plugins/**/vendor/**', '!**/node_modules/**'],
-  testMatch: ['**/js/__tests__/**/*.test.js'],
-  transform: {},
-  testPathIgnorePatterns: ['/node_modules/', '/css/', '/data/', '/audio/', '/effects/', '/fonts/', '/img/', '/movies/', '/save/'],
-};
